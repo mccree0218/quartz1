f6c4b87beaeab0621bff9ca83d3c67f8873d787a
scottliu <scottliu@hitrust.com.tw>
2020-09-01 16:17:41 +0800

    修改註解

--- src/main/java/com/quart/simple/job/MyJobFactory.java
+++ src/main/java/com/quart/simple/job/MyJobFactory.java
@@ -7,14 +7,14 @@ import org.springframework.scheduling.quartz.AdaptableJobFactory;
 
 public class MyJobFactory extends AdaptableJobFactory {
 
-    //这个对象Spring会帮我们自动注入进来,也属于Spring技术范畴.
+    //注入AutowireCapableBeanFactory 於web.xml/applicationContent 內注入
     @Autowired
     private AutowireCapableBeanFactory capableBeanFactory;
     
     protected Object createJobInstance(TriggerFiredBundle bundle) throws Exception {
-        //调用父类的方法
+        //調用父類方法
         Object jobInstance = super.createJobInstance(bundle);
-        //进行注入,这属于Spring的技术,不清楚的可以查看Spring的API.
+        //透過反射機制取得該job實體
         capableBeanFactory.autowireBean(jobInstance);
         return jobInstance;
     }
